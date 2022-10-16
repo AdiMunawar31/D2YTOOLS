d0d91a9f5ac267edbd190bef575e9d61e1a59b67
Adi Munawar <munawar.adi31@gmail.com>
2022-10-16 20:18:15 +0700

    fix bugs dehydratestate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { dehydrate, Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 // import { Hydrate } from 'react-query/hydration';
@@ -9,10 +9,11 @@ import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const [queryClient] = React.useState(() => new QueryClient());
+	const dehydratedState = dehydrate(queryClient);
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Hydrate state={pageProps.dehydratedState}>
+			<Hydrate state={pageProps}>
 				<Component {...pageProps} />
 				<ReactQueryDevtools initialIsOpen={false} />
 			</Hydrate>
